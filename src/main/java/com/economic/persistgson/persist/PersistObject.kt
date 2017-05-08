95e9ff6e1cd204c842baf28b18a83f8dcb3c393f
Tudor Dragan <tudorgk@gmail.com>
2017-05-08 14:46:49 +0200

    Cleaned up repo

---
+++ src/main/java/com/economic/persistgson/persist/PersistObject.kt
@@ -0,0 +1,11 @@
+package com.economic.persistgson.persist
+
+
+/**
+ * Created by Tudor Dragan on 03/05/2017.
+ * Copyright © e-conomic.com
+ */
+
+interface PersistObject {
+    val persistMap: MutableMap<String, Any>
+}
