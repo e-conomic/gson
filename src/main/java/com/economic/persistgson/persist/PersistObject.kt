3664abc4298357b472916b08854a10a716ee3f34
Tudor Dragan <tudorgk@gmail.com>
2017-05-08 15:07:06 +0200

    Added some comments to the PersistObject Interface.

--- src/main/java/com/economic/persistgson/persist/PersistObject.kt
+++ src/main/java/com/economic/persistgson/persist/PersistObject.kt
@@ -6,6 +6,16 @@ package com.economic.persistgson.persist
  * Copyright © e-conomic.com
  */
 
+/**
+ * Defines a generic object that has persist functionality. The purpose of this class
+ * is to construct a default instance of a class that can be used for storing unknown
+ * JSON properties
+ *
+ * */
 interface PersistObject {
+    /**
+     * The [persistMap] is a mutable map that will contain any unknown properties that could not be
+     * mapped to the Model object
+     * */
     val persistMap: MutableMap<String, Any>
 }
